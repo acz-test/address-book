87a679076ce3572c5743906f4c2a8747dceec96e
acz-test <zhou@Andrews-MacBook-Pro-37.local>
2019-02-18 10:52:45 -0800

    Updates

---
+++ precache-manifest.410b67cd84e2abd1dad29fb79a3196b9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/address-book/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "7d5f1059a20d6079c903",
+    "url": "/address-book/static/js/runtime~main.7d5f1059.js"
+  },
+  {
+    "revision": "0780be3789d3b7877aa8",
+    "url": "/address-book/static/js/main.0780be37.chunk.js"
+  },
+  {
+    "revision": "ba1ca5ba3f9c1e3c91ad",
+    "url": "/address-book/static/js/2.ba1ca5ba.chunk.js"
+  },
+  {
+    "revision": "0780be3789d3b7877aa8",
+    "url": "/address-book/static/css/main.e463d741.chunk.css"
+  },
+  {
+    "revision": "7f8c853eb54dff4eab67f08697b263f7",
+    "url": "/address-book/index.html"
+  }
+];
